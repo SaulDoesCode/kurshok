e348f02f8c0f9023c3236e733a0f5e3895b381b0
Saul <saulvdw@gmail.com>
2022-09-19 19:16:53 +0200

    basic personalization

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,36 @@
-<script>
-	export let name;
-</script>
+<script></script>
 
+<nav>
+	<header class="kurshok"> Kurshok </header>
+</nav>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<br>
-	<p>And check out <a href="https://render.com">Render</a>'s Svelte <a href="https://render.com/docs/deploy-svelte">quickstart guide</a> to see how this site was deployed.</p>
-	<p class="large">🧑‍💻</p>
+	<p>
+		Hello, this is a personal site, very much underconstruction
+	</p>
+
+	<footer>
+		<section class="links">
+			<div>
+				<span>github: </span>
+				<a href="http://github.com/SaulDoesCode" target="_blank">SaulDoesCode</a>
+			</div>
+			<div>
+				<span>twitter: </span>
+				<a href="http://twitter.com/saulvdw">@saulvdw</a>
+			</div>
+		</section>
+	</footer>
 </main>
 
 <style>
+	.kurshok {
+		display: inline-block;
+		font-size: 1.3em;
+		background: #ccc;
+		padding: .5em .5em;
+		border-radius: .5em;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
