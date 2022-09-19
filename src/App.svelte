b51a53528aa10326557a31cd2eb0e372018cd660
Saul <saulvdw@gmail.com>
2022-09-19 22:30:38 +0200

    more content

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,18 @@
 	<header class="kurshok"> kurshok </header>
 </nav>
 <main>
+	<br>
 	<section class="books">
 		<header>Books</header>
 		<iframe type="text/html" sandbox="allow-scripts allow-same-origin allow-popups" width="336" height="550" frameborder="0" allowfullscreen style="max-width:100%" src="https://read.amazon.com/kp/card?asin=B08WS1NS1W&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_M3SY9JYDYCMPDW2052H7" ></iframe>
 		<iframe type="text/html" sandbox="allow-scripts allow-same-origin allow-popups" width="336" height="550" frameborder="0" allowfullscreen style="max-width:100%" src="https://read.amazon.com/kp/card?asin=B07ZKXSHLM&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_KG5H5CZMBY22ZK12W5ZY" ></iframe>
 		<iframe type="text/html" sandbox="allow-scripts allow-same-origin allow-popups" width="336" height="550" frameborder="0" allowfullscreen style="max-width:100%" src="https://read.amazon.com/kp/card?asin=B07Z57C9KM&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_9VCET209YES1RV90V3CH" ></iframe>
 	</section>
+	<br>
 </main>
 
 <footer>
-	<section class="links">
+	<section class="footer-section">
 		<header>Links</header>
 		<div>
 			<span>github: </span>
@@ -21,20 +23,33 @@
 			<span>twitter: </span>
 			<a href="http://twitter.com/saulvdw">@saulvdw</a>
 		</div>
+		<br>
+		<div>
+			<a href="https://www.amazon.com/s/ref=dp_byline_sr_ebooks_1?ie=UTF8&field-author=Saul+van+der+Walt&text=Saul+van+der+Walt&sort=relevancerank&search-alias=digital-text">
+					Amazon Books
+			</a>
+		</div>
+	</section>
+
+	<section class="footer-section">
+		<header>Donate</header>
+		<div>
+			<b>ETH: </b> 0x0Bac497c4D77A6eab1531b7341c829400944D30e
+		</div>
 	</section>
 </footer>
 
 <style>
 	:root {
-		--highlight-color: hsl(0, 70%, 60%);
+		--highlight-color: hsl(356, 100%, 66%);
 	}
 	.kurshok {
 		display: inline-block;
-		font-size: 1.3em;
+		font-size: 1.35em;
 		background: var(--highlight-color);
 		color: #fff;
-		padding: .5em .5em;
-		border-radius: .5em;
+		padding: .25em .35em;
+		border-radius: .25em;
 	}
 
 	main {
@@ -46,20 +61,31 @@
 
 	footer {
 		display: inline-block;
-		width: 94vw;
+		display: inline-flex;
+		align-items: flex-start;
+		align-content: center;
+		justify-content: space-between;
+		flex-flow: row wrap;
+		width: 95vw;
 		margin: .5em 2.5vw .5em 2.5vw;
 		padding-top: .5em;
 		border-top: 1px solid var(--highlight-color);
 		box-shadow: inset 0 5px 4px -5px rgb(207, 59, 59, .3);
 	}
 
-	.links {
+	.footer-section {
 		font-size: .8em;
 		display: inline-block;
 		padding: .5em .5em;
 		margin: .5em;
 		border: 1px solid #ccc;
 		border-radius: .25em;
+	}
+
+	.footer-section > header {
+		margin-left: .4em;
+		max-width: 95%;
+		font-size: 1.1em;
 	}
 
 	header {
@@ -70,7 +96,7 @@
 		border-bottom: 1px solid rgba(0,0,0,.15);
 	}
 
-	.links > div {
+	.footer-section > div {
 		margin-left: 1.25em;
 	}
 
